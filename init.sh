0110b4e87d376e993c2bd372f739036f5624a327
Jonathas Bernard Sampaio da Silva <jonathas.sampaio@gmail.com>
2025-03-03 13:56:45 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,11 +8,10 @@ mkdir -p ./elevation_cache
 
 wget https://download.geofabrik.de/south-america/brazil-latest.osm.pbf -P ./files
 
-wget config/vroom -P ./config
-wget config/ors -P ./config
+wget https://github.com/jonathassampaio/vroom-ors/blob/main/config/config.yml -P ./config
+wget https://github.com/jonathassampaio/vroom-ors/blob/main/config/ors-config.yml -P ./config
 
-wget docker-compose.yml
-
+wget https://github.com/jonathassampaio/vroom-ors/blob/main/docker-compose.yml
 
 echo "docker compose up -d"
 
